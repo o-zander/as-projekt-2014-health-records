66f91a3ada758b909e5947b6e740f08807582699
unknown <Konstantin.Dreyer@gmail.com>
2014-10-07 23:02:51 +0200

    removed setsize and lastid, removed integer return for createPatient and Illness, fixed error in SQLDatenhaltung, deleted datenhaltungDB, etc

--- HealthRecords/HealthRecords/IFachkonzept.cs
+++ HealthRecords/HealthRecords/IFachkonzept.cs
@@ -5,22 +5,24 @@ using System.Text;
 
 namespace HealthRecords
 {
-  interface IFachkonzept
-  {
-     Patient[] GetPatients(int setSize, long lastID);
-     Illness[] GetIllnesses(int setSize, long lastID);
-     Patient GetPatient(long patientID);
-     Illness GetIllness(long illnessID);
-     long CreatePatient(Patient patient);
-     long CreateIllness(Illness illness);
-     bool UpdatePatient(Patient patient);
-     bool UpdateIllness(Illness illness);
-     bool LinkPatientIllness(Patient patient,Illness illness);
-     bool UnLinkPatientIllness(Patient patient, Illness illness);
-     bool DeletePatient(Patient patient);
-     bool DeleteIllness(Illness illness);
-     Illness[] GetPatientIllnesses(Patient patient);
-     Patient[] GetIllnessPatients(Illness illness);
+    interface IFachkonzept
+    {
+        Patient[] GetPatients();
+        Illness[] GetIllnesses();
+        Patient[] GetPatients(int setSize, long lastID);
+        Illness[] GetIllnesses(int setSize, long lastID);
+        Patient GetPatient(long patientID);
+        Illness GetIllness(long illnessID);
+        bool CreatePatient(Patient patient);
+        bool CreateIllness(Illness illness);
+        bool UpdatePatient(Patient patient);
+        bool UpdateIllness(Illness illness);
+        bool LinkPatientIllness(Patient patient, Illness illness);
+        bool UnLinkPatientIllness(Patient patient, Illness illness);
+        bool DeletePatient(Patient patient);
+        bool DeleteIllness(Illness illness);
+        Illness[] GetPatientIllnesses(Patient patient);
+        Patient[] GetIllnessPatients(Illness illness);
 
-  }
+    }
 }
